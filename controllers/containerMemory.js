9711156e130bb2f8c954f3f86cd0615af455e54a
Gabriel Hernández <gabrielymarg@gmail.com>
2022-09-09 00:03:01 -0300

    Memory controller added

--- controllers/containerMemory.js
+++ controllers/containerMemory.js
@@ -4,7 +4,7 @@ module.exports = class Container {
     }
 
     // Save data to file
-    save (data) {
+    async save (data) {
         try {
             if (this.container.length > 0) {
                 data.id = this.container.reduce((max, obj) => { return obj.id > max ? obj.id : max }, 0) + 1
@@ -19,7 +19,7 @@ module.exports = class Container {
     }
 
     // Get all data from file, if no data or file return empty array
-    getAll () {
+    async getAll () {
         try {
             if (this.container) {
                 return JSON.parse(this.container)
@@ -32,7 +32,7 @@ module.exports = class Container {
     }
 
     // Get by id
-    getById (id) {
+    async getById (id) {
         try {
             const item = this.container.filter(obj => obj.id === id)
             if (item.length > 0) {
@@ -46,7 +46,7 @@ module.exports = class Container {
     }
 
     // Update by id
-    updateById (id, item) {
+    async updateById (id, item) {
         item.id = id
         try {
             const idx = this.container.findIndex(obj => obj.id === item.id)
@@ -62,27 +62,23 @@ module.exports = class Container {
     }
 
     // Delete by id
-    deleteById (id) {
+    async deleteById (id) {
         try {
-            const result = this.getAll()
-                .then (data => {
-                    const len = data.length
-                    const flt = data.filter(obj => obj.id !== id)
-                    if (len > flt.length) {
-                        this.container = flt
-                        return { success: 'data deleted' }
-                    } else {
-                        return { error: 'data not found' }
-                    }
-                })
-            return result
+            const len = this.container.length
+            const flt = this.container.filter(obj => obj.id !== id)
+            if (len > flt.length) {
+                this.container = flt
+                return { success: 'data deleted' }
+            } else {
+                return { error: 'data not found' }
+            }
         } catch (error) {
             console.log(error)
         }
     }
 
     // Erase all data
-    deleteAll () {
+    async deleteAll () {
         try {
             this.container = []
         } catch (error) {
